b10cfc400cd3d63b5ca447e87ca235256baac245
charles7668 <charles76680119@gmail.com>
2025-11-06 09:06:03 +0800

    Prevent more than one client connection

--- main.go
+++ main.go
@@ -13,12 +13,37 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+var (
+	isClientConnected  bool
+	clientConnectMutex sync.Mutex
+)
+
 func echo(w http.ResponseWriter, r *http.Request) {
+	clientConnectMutex.Lock()
+	if isClientConnected {
+		clientConnectMutex.Unlock()
+		http.Error(w, "A client is already connected", http.StatusConflict)
+		return
+	}
+	isClientConnected = true
+	clientConnectMutex.Unlock()
+
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
+		clientConnectMutex.Lock()
+		// Reset flag on upgrade error
+		isClientConnected = false
+		clientConnectMutex.Unlock()
 		return
 	}
+
+	defer func() {
+		clientConnectMutex.Lock()
+		isClientConnected = false // Reset flag when connection closes
+		clientConnectMutex.Unlock()
+		conn.Close()
+	}()
 
 	var wg sync.WaitGroup
 	done := make(chan struct{})
