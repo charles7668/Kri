7376de7bc660c41f1b86934cf99873779da3c962
charles7668 <charles76680119@gmail.com>
2025-11-06 08:45:45 +0800

    Separate reader and writer goroutines

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
@@ -18,21 +19,55 @@ func echo(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		return
 	}
-	defer conn.Close()
 
-	for {
-		messageType, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
+	var wg sync.WaitGroup
+	done := make(chan struct{})
+	messageChan := make(chan []byte)
+
+	// Reader goroutine
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		// Close messageChan when reader exits
+		defer close(messageChan)
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("read error:", err)
+				// Signal writer to exit
+				close(done)
+				return
+			}
+			log.Printf("Received: %s", message)
+			messageChan <- message
 		}
-		log.Printf("Received: %s", message)
-		err = conn.WriteMessage(messageType, message)
-		if err != nil {
-			log.Println(err)
-			return
+	}()
+
+	// Writer goroutine
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer conn.Close()
+		for {
+			select {
+			case message, ok := <-messageChan:
+				// messageChan was closed by reader
+				if !ok {
+					return
+				}
+				err := conn.WriteMessage(websocket.TextMessage, message)
+				if err != nil {
+					log.Println("write error:", err)
+					return
+				}
+			case <-done: // Reader signaled to exit
+				return
+			}
 		}
-	}
+	}()
+
+	// Wait for both goroutines to finish
+	wg.Wait()
 }
 
 func main() {
